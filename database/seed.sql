484d8a2125016566e5bee338f2a4022e093d7731
unknown <purbojatmiko261@gmail.com>
2026-05-12 14:45:49 +0700

    nyoba koleb database

---
+++ database/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (nama, email, nim, role)
+VALUES
+('Admin', 'admin@gmail.com', '1234567890', 'admin'),
+('User1', 'user1@gmail.com', '0987654321', 'user');
+
+INSERT INTO rewards (nama_reward, poin, gambar)
+VALUES
+('Reward 1', 100, 'reward1.jpg');
